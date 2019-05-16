a1e815d1c30e983bae21c2fe996ca3668a562e1e
Adam Donnison <ajdonnison@gmail.com>
2019-05-16 19:03:50 +1000

    Restructure to support parsing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const parse = require('mailparser').simpleParser
 const AWS = require('aws-sdk')
 const S3 = new AWS.S3({ httpOptions: { timeout: 2000, connectTimeout: 1000 } })
 const SNS = new AWS.SNS()
+const _async = require('async')
 
 exports.handler = function (event, context, callback) {
   var message = event.Records[0].ses
@@ -9,7 +10,7 @@ exports.handler = function (event, context, callback) {
 
   const getRecipient = function (recipientList) {
     console.log('extracting recipient from', recipientList)
-    return recipientList[0].split('@')[0]
+    return recipientList.map(el => el.split('@')[0])
   }
 
   if (message.receipt.dkimVerdict.status === 'FAIL' ||
@@ -17,40 +18,74 @@ exports.handler = function (event, context, callback) {
        message.receipt.spfVerdict.status === 'FAIL' ||
        message.receipt.virusVerdict.status === 'FAIL') {
     console.log('Dropping spam')
-    callback(null, { 'disposition': 'STOP_RULE_SET' })
-  } else {
-    // Strip out the phone number from the recipient
-    let phone = getRecipient(message.mail.destination)
-    console.log(phone)
-    console.log(process.env)
-    S3.getObject({
+    return callback(null, { 'disposition': 'STOP_RULE_SET' })
+  }
+
+  // Needs more work, should look for the first available link
+  // and wrap some text around it.
+  const extractLink = function (text) {
+    let data = text.split(/[\r\n]/)
+    for (let line of data) {
+      if (line.match(/https?:/)) {
+        return line
+      }
+    }
+    return 'You have a message'
+  }
+
+  let phones = getRecipient(message.mail.destination)
+  let emailstruct
+  let parsedEmail
+
+  console.log(phones)
+  _async.series([
+    done => S3.getObject({
       Bucket: process.env.S3_BUCKET,
       Key: message.mail.messageId
     },
     (err, data) => {
       if (err) {
         console.log(err, err.stack)
-        callback(err)
-      } else {
-        console.log('Raw email:\n' + data.Body)
-        parse(data.Body.toString('ascii'))
-          .then((email) => {
-            console.log('Sending SMS')
-            SNS.publish({
-              PhoneNumber: phone,
-              Message: email.text
-            },
-            (err, data) => {
-              if (err) {
-                console.log('failed to send SMS\n', err)
-                callback(err)
-              } else {
-                console.log('Sent SMS', data)
-                callback(null, null)
-              }
-            })
-          })
+        return done(err)
       }
-    })
+      console.log('Raw email:\n' + data.Body)
+      emailstruct = data
+      done()
+    }),
+    done => parse(emailstruct.Body.toString('ascii'), (err, data) => {
+      if (err) {
+        console.log(err)
+        return done(err)
+      }
+      parsedEmail = extractLink(data.text)
+      done()
+    }),
+    done => {
+      _async.eachOf(phones, (phone, done) => {
+        SNS.publish({
+          PhoneNUmber: phone,
+          Message: parsedEmail
+        },
+        (err, data) => {
+          if (err) {
+            console.log('failed to send SMS\n', err)
+            return done(err)
+          }
+          console.log(data)
+          done()
+        })
+      },
+      err => {
+        done(err)
+      })
+    }
+  ],
+  err => {
+    if (err) {
+      console.log(err)
+      return callback(err)
+    }
+    callback(null, null)
   }
+  )
 }
